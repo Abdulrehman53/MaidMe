a578b8fc81089f14ea45a0f200284db401869688
Abdul Rehman <abdulrehman.dev@gmail.com>
2019-07-15 20:15:47 +0500

    First commit

--- build/ios/Debug-iphonesimulator/flutter_country_picker/flutter_country_picker.framework/Headers/FlutterCountryPickerPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface FlutterCountryPickerPlugin : NSObject<FlutterPlugin>
-@end
--- ios/Pods/Target Support Files/flutter_country_picker/flutter_country_picker-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_flutter_country_picker : NSObject
-@end
-@implementation PodsDummy_flutter_country_picker
-@end
